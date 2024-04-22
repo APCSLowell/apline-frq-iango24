c9123f16578d5e39cd35ecf73e7703389a2694c9
iango24 <143123742+iango24@users.noreply.github.com>
2024-04-22 11:25:44 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,11 +1,18 @@
 public class APLine
 {
-  public APLine(int a, int b, int c){}
+  int aa=0;
+  int bb=0;
+  int cc=0;
+  public APLine(int a, int b, int c){
+    int aa = a;
+    int bb = b;
+    int cc = c;
+  }
   public double getSlope(){
     return -a/b;
   } 
   public boolean isOnLine(int x, int y){
-    if(a*x+b*y+c==0)
+    if(aa*x+bb*y+cc==0)
         return true;
     return false;
   }
